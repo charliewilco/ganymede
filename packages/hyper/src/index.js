1762aa12c5bd454b9c8ef7d7efcc31e18283b8d1
Charles ⚡ <charlespeters42@gmail.com>
2017-06-29 20:49:32 -0700

    Adding darker background to hyper

--- packages/hyper/src/index.js
+++ packages/hyper/src/index.js
@@ -2,7 +2,7 @@ import { uiGroups, ansiGroups } from 'ganymede'
 
 // rgba(9, 55, 80, 1) = ui.background
 
-const AlphaBkg = 'rgba(9, 55, 80, 0.875)'
+const AlphaBkg = 'rgba(9, 55, 80, 0.925)'
 
 exports.onWindow = browserWindow => browserWindow.setVibrancy('dark')
 exports.decorateConfig = (config) => Object.assign({}, config, {
